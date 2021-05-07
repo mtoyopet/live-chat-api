18c00d8f3f5bf41bb798491f2980e46db5ab2cbf
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-05-07 13:49:30 +0900

    indexを作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,15 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.includes(:user).all
+    mesages_array = @messages.map do |message|
+      {
+        user_id: message.user.id,
+        name: message.user.name,
+        content: message.content,
+        created_at: message.created_at
+      }
+    end
 
-    render :json => @messages, :status => :ok
+    render :json => mesages_array, :status => :ok
   end
 end
